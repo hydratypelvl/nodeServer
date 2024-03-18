763d9a70d5543b9e9c265743f67fd4c703824bae
RR <rihards.adm@gmail.com>
2024-03-18 03:08:38 +0200

    fix

--- api/index.ts
+++ api/index.ts
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 
 // Middleware to enable CORS
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://your-next-js-app.com'); // Replace with your Next.js app's domain
+  res.setHeader('Access-Control-Allow-Origin', 'http://nextvercelapi.vercel.app'); // Replace with your Next.js app's domain
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
